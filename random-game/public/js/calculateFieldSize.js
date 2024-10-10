1178d965caddb5ffbb393485d0614cd9ab2f941a
Anastasiia Nikonova <anastaney@gmail.com>
2024-10-10 21:48:11 +0200

    fix: fix background-houses

--- random-game/public/js/calculateFieldSize.js
+++ random-game/public/js/calculateFieldSize.js
@@ -2,8 +2,8 @@ export function calculateFieldSize(direction = "width") {
   const cellSize = 30;
   const freeSpacePercentage = 5;
   const screenResolution =
-    direction === "height" ? window.innerHeight * 0.7 : window.innerWidth;
-  const padding = direction === "height" ? 30 : 80;
+    direction === "height" ? window.innerHeight - 256 : window.innerWidth;
+  const padding = direction === "height" ? 30 : 96;
   const freeSpace = screenResolution * (freeSpacePercentage / 100);
   const availableSpace = screenResolution - 2 * freeSpace - 2 * padding;
   const cellCount = Math.floor(availableSpace / cellSize);
